29c14e56179d9a50fe6f426ea50b9a2d5b7efc8f
Joshua Flancer <josh@joshuas-mbp-2.attlocal.net>
2018-12-20 01:09:41 -0800

    Apparently writing to a log file caused issues

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const auth = require('./auth');
 const path = require('path');
-const fs = require('fs');
+// const fs = require('fs');
 
 const bot = new Discord.Client();
 
-const logFile = fs.createWriteStream('./uses.log', {
-  flags: 'a',
-});
+// const logFile = fs.createWriteStream('./uses.log', {
+//   flags: 'a',
+// });
 
 bot.on('message', async (msg) => {
   // then this is just a normal message, don't do anything
@@ -15,22 +15,15 @@ bot.on('message', async (msg) => {
     return;
   }
 
-  if (msg.content === '!mitchhelp') {
-    let helpString = '';
-    const files = await fs.readdir(path.join(__dirname, 'cash'));
-
-    return;
-  }
-
-  const { voiceChannel } = msg.member;
-
-  if (!voiceChannel) {
+  if (!msg.member.voiceChannel) {
     return;
   }
 
   // take out exclamation mark
   const cmd = msg.content.substring(7);
 
+
+  const { voiceChannel } = msg.member;
   const conn = await voiceChannel.join();
   const pathName = path.join(__dirname, `cash/${cmd}.mp3`);
 
@@ -38,15 +31,14 @@ bot.on('message', async (msg) => {
 
   dispatcher.on('end', () => {
     voiceChannel.leave();
-    const { id, nickname, displayName } = msg.member;
-    logFile.write(`[${new Date().toISOString()}] ${cmd} - ${id}:${nickname}:${displayName}\n`);
+    // const { id, nickname, displayName } = msg.member;
+    // logFile.write(`[${new Date().toISOString()}] ${cmd} - ${id}:${nickname}:${displayName}\n`);
   });
 });
 
 // ensure closing file
-process.on('exit', () => {
-  logFile.close();
-});
+// process.on('exit', () => {
+//   logFile.close();
+// });
 
 bot.login(auth.token);
-
